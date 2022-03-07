523382752c43ee5f16f77b45fd61702cbee6d61d
bonkab.koo <bonkab.koo@lge.com>
2022-03-07 10:55:57 +0900

    [MBSU] add empty action
    Change-Id: Ia4b491a896009b0239d353d0c53c92089877b31c

--- src/components/drawer/MainDrawer.js
+++ src/components/drawer/MainDrawer.js
@@ -2,23 +2,41 @@ import MailIcon from "@mui/icons-material/Mail";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
 
+const action1 = () => {
+  alert("action 1");
+};
+const action2 = () => {
+  alert("action 2");
+};
+const action3 = () => {
+  alert("action 3");
+};
+const action4 = () => {
+  alert("action 4");
+};
+const action5 = () => {
+  alert("action 5");
+};
+const items = [
+  {text:"TV에서 보기", action:action1},
+  {text:"TV와 연결", action:action2},
+  {text:"모닝브리핑 켜기", action:action3},
+  {text:"TV 끄기", action:action4},
+  {text:"모바일에서 보기", action:action5},
+];
+
 export const MainDrawer = {
   title: "미리보기",
   list: (
     <List sx={{ py: 0 }}>
-      {[
-        "TV에서 보기",
-        "TV와 연결",
-        "모닝브리핑 켜기",
-        "TV 끄기",
-        "모바일에서 보기",
-      ].map((text, index) => (
-        <ListItem button key={text}>
+      {items.map((item, index) => (
+        <ListItem button key={item.text}
+          onClick={item.action}>
           <ListItemIcon>
             {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
           </ListItemIcon>
           <ListItemText
-            primary={text}
+            primary={item.text}
             secondary={index === 0 ? "UJ6600" : ""}
           />
         </ListItem>
