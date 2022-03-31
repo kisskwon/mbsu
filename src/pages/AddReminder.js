a48a4a2c8ee9a5c4bfdcd67167096bc43898ca55
kiss.kwon <kiss.kwon@lge.com>
2022-03-31 16:21:17 +0900

    [MBSU] modify coding rule and init setting
    ip 주소 변경시 client key 초기화 하도록 수정
    
    Change-Id: I85b43831c0d498a3658b4aee99d1de46d690e02d

--- src/pages/AddReminder.js
+++ src/pages/AddReminder.js
@@ -2,7 +2,14 @@ import styled from "@emotion/styled";
 import { Paper, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
-import { collection, doc, getDocs, onSnapshot, setDoc, writeBatch } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  onSnapshot,
+  setDoc,
+  writeBatch,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
@@ -24,8 +31,10 @@ function AddReminder(props) {
   const navigate = useNavigate();
   const state = useLocation().state;
   const mode = state?.mode || REMINDER_MODE.NORMAL;
-  const defaultUrl = "https://www.netflix.com/kr/title/81517168?s=a&trkid=13747225&t=cp&vlang=ko&clip=81564946"; //25 21
-  const webDefaulturl = "https://namu.wiki/w/%EA%B9%80%EA%B1%B4%ED%9D%AC/%EB%85%BC%EB%9E%80#s-2";
+  const defaultUrl =
+    "https://www.netflix.com/kr/title/81517168?s=a&trkid=13747225&t=cp&vlang=ko&clip=81564946"; //25 21
+  const webDefaulturl =
+    "https://namu.wiki/w/%EA%B9%80%EA%B1%B4%ED%9D%AC/%EB%85%BC%EB%9E%80#s-2";
   const netflixData = useRecoilValue(NetflixData);
   const webCrawlData = useRecoilValue(WebCrawlData);
   const [showProgress, setShowProgress] = useState(false);
@@ -37,17 +46,17 @@ function AddReminder(props) {
       console.log("existActivity try");
       navigator.app.exitApp();
       console.log("existActivity try 2222");
-    } catch(e) {
+    } catch (e) {
       console.log("existActivity catch e :" + e);
       navigate(-1);
       console.log("existActivity catch2222 e :" + e);
     }
-  }
+  };
 
   const handleSave = () => {
     let type = "shopping";
     let data = {
-      summary : webCrawlData.customDescription !== "" ? webCrawlData.customDescription : webCrawlData.description,
+      summary: webCrawlData.description,
       title: webCrawlData.title,
       url: webCrawlData.url,
     };
@@ -58,30 +67,40 @@ function AddReminder(props) {
         summary: netflixData.summary,
         title: netflixData.title,
         titleid: netflixData.titleid,
-        url: netflixData.url
+        url: netflixData.url,
       };
     }
 
     setShowProgress(true);
-    tvControlUtil.launchOneshotOverlay(type, () => {
-      const batch = writeBatch(db);
-      const messageTypeRef = doc(db, "thinq_talk", "message_type");
-      batch.set(messageTypeRef, { type: type });
+    tvControlUtil.launchOneshotOverlay(
+      type,
+      () => {
+        const batch = writeBatch(db);
+        const messageTypeRef = doc(db, "thinq_talk", "message_type");
+        batch.set(messageTypeRef, { type: type });
 
-      const contentsRef = doc(db, "thinq_talk", "contents");
-      batch.set(contentsRef, data);
+        const contentsRef = doc(db, "thinq_talk", "contents");
+        batch.set(contentsRef, data);
 
-      const shoppingMode = mode == REMINDER_MODE.NORMAL;
+        const shoppingMode = mode == REMINDER_MODE.NORMAL;
 
-      const playRef = doc(db, "thinq_talk", shoppingMode ? "browser": "netflix");
-      batch.set(playRef, shoppingMode ? {show: false} : {play: false});
-      batch.commit();
-    }, () => {
-      console.log("launchOneshotOverlay error...");
-      window.cordova?.plugins?.TVConnect.toast("TV 연결에 실패하여 종료합니다.");
+        const playRef = doc(
+          db,
+          "thinq_talk",
+          shoppingMode ? "browser" : "netflix"
+        );
+        batch.set(playRef, shoppingMode ? { show: false } : { play: false });
+        batch.commit();
+      },
+      () => {
+        console.log("launchOneshotOverlay error...");
+        window.cordova?.plugins?.TVConnect.toast(
+          "TV 연결에 실패하여 종료합니다."
+        );
 
-      existActivity();
-    });
+        existActivity();
+      }
+    );
   };
 
   useEffect(() => {
@@ -90,7 +109,11 @@ function AddReminder(props) {
       const querySnapshot = await getDocs(collection(db, "thinq_talk"));
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
-        console.log("----------------------DOCUMENT NAME ", doc.id, "----------------------");
+        console.log(
+          "----------------------DOCUMENT NAME ",
+          doc.id,
+          "----------------------"
+        );
         console.table(doc.data());
       });
     };
@@ -128,32 +151,33 @@ function AddReminder(props) {
 
   return (
     <>
-      <MBAppBar title={"알림이 추가"} sub reminder/>
+      <MBAppBar title={"알림이 추가"} sub reminder />
       <StyledPaper square>
         <div style={{ padding: "20px", backgroundColor: "#272727" }}>
           <Typography variant="h5" color="#90caf9" fontWeight="bold">
-          {showProgress === true ? "TV에 전송중입니다. 잠시만 기다려주세요":"알림 받을 내용을 추가하세요."}
+            {showProgress === true
+              ? "TV에 전송중입니다. 잠시만 기다려주세요"
+              : "알림 받을 내용을 추가하세요."}
           </Typography>
         </div>
 
         <MBSubCard>
-          {showProgress === true ?
+          {showProgress === true ? (
             <div
               style={{
                 display: "grid",
                 width: "90%",
-                height:"500",
+                height: "500",
                 margin: "auto",
                 marginBottom: "15px",
                 marginTop: "15px",
                 display: "flex",
-                justifyContent: "center"
+                justifyContent: "center",
               }}
             >
-              <Lottie/>
+              <Lottie />
             </div>
-
-          :  mode === REMINDER_MODE.NETFLIX ? (
+          ) : mode === REMINDER_MODE.NETFLIX ? (
             <div
               style={{
                 display: "grid",
@@ -177,41 +201,44 @@ function AddReminder(props) {
             >
               <WebInformation url={state?.url || webDefaulturl} />
             </div>
-
           )}
         </MBSubCard>
       </StyledPaper>
-      {showProgress === true ?
-      <div></div>
-      :
-      <Stack
-        direction="row"
-        justifyContent="space-evenly"
-        alignItems="center"
-        spacing={2}
-        sx={{
-          position: "fixed",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          minHeight: 56,
-          bgcolor: "#1d1d1d",
-        }}
-      >
-        <Button
-          variant="text"
-          sx={{ fontWeight: "bold" }}
-          onClick={() => {
-            existActivity();
+      {showProgress === true ? (
+        <div></div>
+      ) : (
+        <Stack
+          direction="row"
+          justifyContent="space-evenly"
+          alignItems="center"
+          spacing={2}
+          sx={{
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            minHeight: 56,
+            bgcolor: "#1d1d1d",
           }}
         >
-          취소
-        </Button>
-        <Button variant="text" sx={{ fontWeight: "bold" }} onClick={handleSave}>
-          저장
-        </Button>
-      </Stack>
-      }
+          <Button
+            variant="text"
+            sx={{ fontWeight: "bold" }}
+            onClick={() => {
+              existActivity();
+            }}
+          >
+            취소
+          </Button>
+          <Button
+            variant="text"
+            sx={{ fontWeight: "bold" }}
+            onClick={handleSave}
+          >
+            저장
+          </Button>
+        </Stack>
+      )}
     </>
   );
 }
