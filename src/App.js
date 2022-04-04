a08aceda3b51b49043658313551d05e081e6b6d6
yr.ahn <yr.ahn@lge.com>
2022-04-04 11:07:48 +0900

    [MBSU] remove unnecessary menu
    [SV]
    [DESC]
     - 사용되지 않는 메뉴 삭제 (알람 설정은 혹시 몰라 주석처리함)
    
    Change-Id: Iebe3c6c839afd9b8983db81fc6958edf6a826f6c

--- src/App.js
+++ src/App.js
@@ -6,19 +6,10 @@ import ScrollToTop from "./components/ScrollToTop";
 import FirebaseListener from "./firebase/FirebaseListener";
 import AddReminder from "./pages/AddReminder";
 import AlarmSetting from "./pages/AlarmSetting";
-import Calendar from "./pages/Calendar";
-import Car from "./pages/Car";
-import EditGreeting from "./pages/EditGreeting";
 import Gallery from "./pages/Gallery";
 import Home from "./pages/Home";
-import Memo from "./pages/Memo";
-import MemoGuide from "./pages/MemoGuide";
-import NaviSetting from "./pages/NaviSetting";
-import RegionSearch from "./pages/RegionSearch";
 import Reminder from "./pages/Reminder";
-import Slogan from "./pages/Slogan";
 import Talk from "./pages/Talk";
-import Weather from "./pages/Weather";
 
 const darkTheme = createTheme({
   palette: {
@@ -65,17 +56,8 @@ const AppRoutes = () => {
   const routes = useRoutes([
     { path: "/", element: <Home /> },
     { path: "/alarm", element: <AlarmSetting /> },
-    { path: "/weather", element: <Weather /> },
-    { path: "/navi", element: <NaviSetting /> },
-    { path: "/memo", element: <Memo /> },
-    { path: "/calendar", element: <Calendar /> },
-    { path: "/car", element: <Car /> },
-    { path: "/slogan", element: <Slogan /> },
     { path: "/talk", element: <Talk /> },
-    { path: "/regionSearch", element: <RegionSearch /> },
     { path: "/gallery", element: <Gallery /> },
-    { path: "/memoGuide", element: <MemoGuide /> },
-    { path: "/editGreeting", element: <EditGreeting /> },
     { path: "/reminder", element: <Reminder /> },
     { path: "/addReminder", element: <AddReminder /> },
   ]);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -53,9 +53,9 @@ function Home(props) {
 
   return (
     <>
-      <MBAppBar title={"모닝브리핑 블랙"} />
+      <MBAppBar title={"ThinQ stand-alone"} />
       <StyledPaper square>
-        <MBCard title="알림 설정" action="알림" to="/alarm">
+        {/* <MBCard title="알림 설정" action="알림" to="/alarm">
           <CardContent sx={{ py: 1 }}>
             <Typography sx={{ fontSize: 30 }}>{getTime()}</Typography>
             <Typography
@@ -66,65 +66,11 @@ function Home(props) {
               {getDays()}
             </Typography>
           </CardContent>
-        </MBCard>
+        </MBCard> */}
         <MBCard action="토크" to="/talk">
           <CardContent>
             <Typography variant="subtitle2" color={"text.secondary"}>
               띵큐토크
-            </Typography>
-          </CardContent>
-        </MBCard>
-        {/* <MBCard action="알림이" to="/addReminder">
-          <CardContent>
-            <Typography variant="subtitle2" color={"text.secondary"}>
-              띵큐 알림이
-            </Typography>
-          </CardContent>
-        </MBCard> */}
-        <MBCard title="기본 위젯 설정" action="날씨" to="/weather">
-          <CardContent>
-            <Typography variant="subtitle2" color={"text.secondary"}>
-              '논현동'의 날씨를 확인할 수 있어요.
-            </Typography>
-          </CardContent>
-        </MBCard>
-        <MBCard
-          title="선택 위젯 설정"
-          subtitle="2개 이상의 위젯을 선택해주세요."
-          action="교통"
-          to="/navi"
-        >
-          <CardContent>
-            <Typography variant="subtitle2" color={"text.secondary"}>
-              마곡동에서 압구정동까지 가장 빠른길을 확인할 수 있어요.
-            </Typography>
-          </CardContent>
-        </MBCard>
-        <MBCard action="메모" to="/memo">
-          <CardContent>
-            <Typography variant="subtitle2" color={"text.secondary"}>
-              등록한 4개의 메모를 확인할 수 있어요.
-            </Typography>
-          </CardContent>
-        </MBCard>
-        <MBCard action="일정" to="/calendar">
-          <CardContent>
-            <Typography variant="subtitle2" color={"text.secondary"}>
-              등록한 5개의 일정을 확인할 수 있어요.
-            </Typography>
-          </CardContent>
-        </MBCard>
-        <MBCard action="차량" to="/car">
-          <CardContent>
-            <Typography variant="subtitle2" color={"text.secondary"}>
-              "홍길동의 차" 주차위치와 차량정보를 확인할 수 있어요.
-            </Typography>
-          </CardContent>
-        </MBCard>
-        <MBCard title="공통설정" action="대표 문구" to="/slogan">
-          <CardContent>
-            <Typography variant="subtitle2" color={"text.secondary"}>
-              Good morning, Gildong! Have a nice day!
             </Typography>
           </CardContent>
         </MBCard>
